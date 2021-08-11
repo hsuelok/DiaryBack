ec79c2b5bc0b9cd0ed59c128814c6640e73a9345
HsuelokMBP <hsuelok@icloud.com>
2021-08-12 00:01:22 +0800

    .

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 {
     //需要修改的数据库信息,登录名,密码,库名
     string user = "root";
-    string passwd = "root1234";
+    string passwd = "root123";
     string databasename = "qgydb";
 
     //命令行解析
